bc368806ffa49596f4dfd395ac4442dda68cb3ed
Daniel Baykov <danibaikov@gmail.com>
2023-08-23 17:41:37 +0300

    adding @MappedSuperclass annotation in product entity

--- src/main/java/baykov/daniel/fooddelivery/domain/entity/Product.java
+++ src/main/java/baykov/daniel/fooddelivery/domain/entity/Product.java
@@ -1,6 +1,7 @@
 package baykov.daniel.fooddelivery.domain.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,6 +9,7 @@ import java.math.BigDecimal;
 
 @Getter
 @Setter
+@MappedSuperclass
 public abstract class Product extends BaseEntity {
 
     @Column(nullable = false)
