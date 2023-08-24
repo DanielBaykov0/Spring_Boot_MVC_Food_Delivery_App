f4a3759372d877d6e0846e7d5f3cb7ac969c10bc
Daniel Baykov <danibaikov@gmail.com>
2023-08-24 09:47:55 +0300

    adding food categories and their page

--- src/main/java/baykov/daniel/fooddelivery/web/MenuController.java
+++ src/main/java/baykov/daniel/fooddelivery/web/MenuController.java
@@ -1,7 +1,10 @@
 package baykov.daniel.fooddelivery.web;
 
+import baykov.daniel.fooddelivery.domain.constant.FoodCategoryEnum;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class MenuController {
@@ -9,5 +12,13 @@ public class MenuController {
     @GetMapping("/menu")
     public String getMenu() {
         return "menu-categories";
+    }
+
+    @GetMapping("/menu/{category}")
+    public String getCategoryPage(
+            @PathVariable FoodCategoryEnum foodCategoryEnum,
+            Model model) {
+        model.addAttribute("category", foodCategoryEnum);
+        return "category-page";
     }
 }
