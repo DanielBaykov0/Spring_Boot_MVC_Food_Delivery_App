70f460c401453eab423f21aa99256fa5f024b64c
Daniel Baykov <danibaikov@gmail.com>
2023-08-28 23:05:27 +0300

    implementing remove from cart functionality and fixing add to cart

--- src/main/java/baykov/daniel/fooddelivery/web/CartController.java
+++ src/main/java/baykov/daniel/fooddelivery/web/CartController.java
@@ -8,28 +8,35 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
 
 @Controller
 @AllArgsConstructor
+@RequestMapping("/cart")
 public class CartController {
 
     private final CartService cartService;
     private final ProductService productService;
     private final OrderService orderService;
 
-    @GetMapping("/cart")
+    @GetMapping
     public String getCart(Model model, Principal principal) {
         model.addAttribute("cartProducts", this.orderService.getProducts(principal));
         return "order-cart";
     }
 
-    @GetMapping("/cart/add/{id}")
-    public String addToCart(
-            @PathVariable Long id,
-            Principal principal) {
+    @GetMapping("/add/{id}")
+    public String addToCart(@PathVariable Long id, Principal principal) {
         String category = this.productService.getProductCategory(id);
+        this.cartService.addToCart(id, principal);
         return "redirect:/menu" + category;
+    }
+
+    @GetMapping("/remove/{id}")
+    public String removeFromCart(@PathVariable Long id, Principal principal) {
+        this.cartService.removeFromCart(id, principal);
+        return "redirect:/cart";
     }
 }
